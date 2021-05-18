559011fa4f4896ecd44f2e5159a46df13fd33de5
vinodhinivijayan <44034881+vinodhinivijayan@users.noreply.github.com>
2021-05-18 10:37:46 -0400

    Update backend.tf

--- database/terraform/backend.tf
+++ database/terraform/backend.tf
@@ -1,13 +1,16 @@
+variable "bucket_name" {}
+variable key_name{}
 terraform {
   # DigitalOcean uses the S3 spec.
   backend "s3" {
-    bucket = "treetracker-production-terraform"
-    key    = "terraform-database.tfstate"
+    bucket = "treetracker-dev-terraform"
+    key    = "var.key_name"
     endpoint = "https://sfo2.digitaloceanspaces.com"
     # DO uses the S3 format
     # eu-west-1 is used to pass TF validation
     # Region is ACTUALLY sfo2 on DO
     region = "eu-west-1"
+    
     # Deactivate a few checks as TF will attempt these against AWS
     skip_credentials_validation = true
     # skip_get_ec2_platforms = true
